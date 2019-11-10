d050f064826e5c16f003f8ed4bc5f057ae84bfd2
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-11-10 22:03:49 +0100

    add finding the average in a loop

--- pr05_04_loop.py
+++ pr05_04_loop.py
@@ -1,20 +1,18 @@
 # pr05_04_loop.py
 
 # counting in a loop
-
 count = 0
-print('Before:', count)
+print('Before counting:', count)
 for number in [9, 41, 12, 3, 74, 15]:
     count += 1
     print(str(count) + '. ' + str(number))
 
-print('After:', count)
+print('After counting:', count)
 print('\n')
 
 
 
 # summing in a loop
-
 sum = 0
 print('SUM before:', sum)
 for number in [9, 41, 12, 3, 74, 15]:
@@ -22,3 +20,17 @@ for number in [9, 41, 12, 3, 74, 15]:
     print(str(sum) + ' <-- ' + str(number))
 
 print('SUM after:', sum)
+print('\n')
+
+
+# finding the average in a loop
+count = 0
+sum = 0
+print('Count and SUM before:', str(count) + ',', sum)
+
+for number in [9, 41, 12, 3, 74, 15]:
+    count += 1
+    sum = sum + number
+    print(str(count) + '.: SUM', str(sum) + ' <-- number ' + str(number))
+
+print('Count, SUM and average after: ' + str(count) + ', ' + str(sum) + ', ' + str(round(sum / count, 1)))     # rounding is not in example
