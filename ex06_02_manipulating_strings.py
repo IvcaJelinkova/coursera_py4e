1a1fbc99b3cbe24ffe0dfbf3657b3f7b19472d51
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-11-12 18:40:34 +0100

    new file

---
+++ ex06_02_manipulating_strings.py
@@ -0,0 +1,14 @@
+# ex06_02_manipulating_strings.py
+
+# using "in" as a logical operator
+fruit = 'BANANA'
+'n' in fruit        # --> True
+
+if 'a' in fruit:
+    print('Found it!')      # when if is True --> Found it!
+
+# A < a
+print(fruit.capitalize())
+print(fruit.center(3))
+
+
