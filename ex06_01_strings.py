012ab8dccefce95e28c09060e367811305f0fd64
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-11-12 18:02:19 +0100

    nicer

--- ex06_01_strings.py
+++ ex06_01_strings.py
@@ -5,9 +5,24 @@ fruit = 'banana'
 for letter in fruit:
     print(letter)
 
+print()
 
+
+# metoda 2 (not so nice/clever):
 index = 0
 while index < len(fruit):
     letter = fruit[index]
     print(str(index) + '. index pro písmeno: ' + str(letter))
     index += 1
+
+print()
+
+
+# looping and counting:
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count += 1
+
+print('Počet výskytů písmene "a": ' + str(count))
