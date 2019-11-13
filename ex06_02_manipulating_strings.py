9ee83a5dd85d55d9b7aa6b468d4edafcd5770564
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-11-13 20:38:56 +0100

    testing str.metods

--- ex06_02_manipulating_strings.py
+++ ex06_02_manipulating_strings.py
@@ -4,11 +4,18 @@
 fruit = 'BANANA'
 'n' in fruit        # --> True
 
-if 'a' in fruit:
+if 'A' in fruit:
     print('Found it!')      # when if is True --> Found it!
 
 # A < a
 print(fruit.capitalize())
-print(fruit.center(3))
+print(fruit.center(25, 'b'))    # vysadí 25-6 béček a doprostřed vsadí BANANA
+print(fruit.endswith('NA'))     # True/False if str ends with 'NA'
+print(fruit.find('A'))          # return index of the first position char 'A'
+print(fruit.lstrip('B'))        # remove 'B' from left
+print(fruit.replace('NA', 'NO', 1))     # replace 'NO' insted of 'NA' in the first place
+print(fruit.strip('BA'))        # --> NAN
+
+
 
 
