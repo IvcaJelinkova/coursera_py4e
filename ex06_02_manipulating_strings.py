7ae81b47bc0a09bda44da89597c958cac5700aff
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-11-14 16:27:59 +0100

    add parsing adn extracting with strings

--- ex06_02_manipulating_strings.py
+++ ex06_02_manipulating_strings.py
@@ -17,5 +17,17 @@ print(fruit.replace('NA', 'NO', 1))     # replace 'NO' insted of 'NA' in the fir
 print(fruit.strip('BA'))        # --> NAN
 
 
+# parsing and extracting
+data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+atpos = data.find('@')
+print(atpos)                    # --> 21 (index '@')
+
+sppos = data.find(' ', atpos)
+print(sppos)                    # --> 31 (index ' ' after '@' (where to start))
+
+host = data[atpos + 1 : sppos]
+print(host)                     # --> uct.ac.za
+
+
 
 
