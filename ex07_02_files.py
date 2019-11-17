ed76eda4f987c7628461b06debf97117e247df4b
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-11-17 12:28:29 +0100

    add reading file and searching through a file

--- ex07_02_files.py
+++ ex07_02_files.py
@@ -1,7 +1,18 @@
 # ex07_02_files.py
 
-fhandle = open('mbox.txt')
-count = 0
+fhandle = open('m_box.txt')
+# count = 0
+# for line in fhandle:
+#     count += 1
+# print('Line Count =', count)
+
+# 2) reading the "whole" file:
+# inp = fhandle.read()
+# print(inp)
+# print(len(inp))
+# print(inp[:10])
+
+# 3) searching through a file:
 for line in fhandle:
-    count += 1
-print('Line Count =', count)
+    if line.startswith('line'):
+        print(line)                 # print printing new line also
