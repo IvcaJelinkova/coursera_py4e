c9b4c63497b07f5cd8760e234443eeda6757dbe6
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-11-23 08:51:33 +0100

    add counting with get(), two iteration, bigword + counts

--- ex09_dictionaries.py
+++ ex09_dictionaries.py
@@ -5,4 +5,34 @@ print(jjj)
 
 print('fred' in jjj)        # --> True
 
+# simplified counting with get()
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names:
+    counts[name] = counts.get(name, 0) + 1
+print(counts)
 
+
+# two iteration
+for name, number in counts.items():
+    print(name, number)
+
+
+#  bigword + counts
+name = input('Enter file: ')
+fhandle = open(name)
+
+counts = dict()
+for line in fhandle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+bigcount = None
+bigword = None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
