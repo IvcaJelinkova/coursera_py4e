bdcfe1723a322df73a76df108515cd2f56f391b4
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-11-23 09:37:38 +0100

    counting the greatest number of mail in file

---
+++ hw09_dictionaries_greatest_number_of_mail.py
@@ -0,0 +1,37 @@
+#9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail
+# messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent
+# the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number
+# of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using
+# a maximum loop to find the most prolific committer.
+
+
+fhandle = open('mbox-short.txt')
+
+mails = dict()
+
+for line in fhandle:
+    if line.startswith('From '):
+        words = line.split()
+        #print(words[1])
+        mail = words[1]
+        mails[mail] = mails.get(mail, 0) + 1
+#print(mails)
+
+bigword = None
+bigcount = None
+
+for mail, count in mails.items():
+    if bigword is None or bigcount < 1:
+        bigword = mail
+        bigcount = count
+    elif bigcount < count:
+        bigcount = count
+        bigword = mail
+
+
+print(bigword, bigcount)
+
+
+
+
+
