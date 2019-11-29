d6756b613dc45a2a93f581012ae656d7b41074b3
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-11-29 19:50:38 +0100

    add easy sorting by key

--- hw10_tuples_message_distribution_by_hours.py
+++ hw10_tuples_message_distribution_by_hours.py
@@ -3,6 +3,12 @@
 #     a second time using a colon.
 # From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 # Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
+# result:
+# 04 3
+# 06 1
+# 07 1
+# 09 2
+# ...
 
 
 fname = input('Enter file: ')
@@ -19,14 +25,21 @@ for line in fhandle:
         hour = words[5][:2]
         hours[hour] = hours.get(hour, 0) + 1    # have hour and its count in dict()
 
-
-list_hours = list()
-for hour, count in hours.items():
-    list_hours.append((hour, count))
-list_hours = sorted(list_hours)
-
-for hour, count in list_hours:
+# sorted by key (hour):
+touple_hours = sorted(hours.items())
+for hour, count in touple_hours:
     print(hour, count)
+
+
+# OR:
+
+# list_hours = list()
+# for hour, count in hours.items():
+#     list_hours.append((hour, count))
+# list_hours = sorted(list_hours)
+#
+# for hour, count in list_hours:
+#     print(hour, count)
 
 
 
