1983d7e1efcaf146670d623137574c9a889b5ddc
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-12-01 21:02:16 +0100

    max of spam confidence

--- ex11_regular_expressions.py
+++ ex11_regular_expressions.py
@@ -42,4 +42,17 @@ y = re.findall('^From .*@([^ ]*)', line)
 print('tuning host:', y)
 
 
+# 6) Spam Confidence:
+handle = open('mbox-short.txt')
+numlist = list()
+for line in handle:
+    line = line.rstrip()
+    stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)
+    if len(stuff) != 1:
+        continue
+    num = float(stuff[0])
+    numlist.append(num)
+print('Maximum:', max(numlist))
+
+
 
