0384039d04c692ce755b4e47a8331b5706afebd9
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-12-01 12:38:57 +0100

    print list of strings from text + search for mail adresses

--- ex11_regular_expressions.py
+++ ex11_regular_expressions.py
@@ -15,7 +15,15 @@ print()
 # 2) print list of strings from text:
 x = 'My 2 favorite number are 19 and 42. '
 y = re.findall('[0-9]+', x)
-print(y)
+print('Faforite number:', y)
+
+
+# 3) search for mail adresses:
+x = 'From stephen.marquard@uct.ac.za Sat Jan   5 09:16:14 2008'
+y = re.findall('\S+@\S+', x)
+print('Mail:', y)
+
+
 
 
 
