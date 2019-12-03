fa4df4b4518a13493b61af1ba04f684fd139d210
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-12-03 17:54:04 +0100

    open and close file, list comprehension optional after

--- hw11_re_finding_numbers_in_file.py
+++ hw11_re_finding_numbers_in_file.py
@@ -4,40 +4,42 @@ import re
 
 fname = input('Enter file name: ')
 if len(fname) < 1:
-    fname = 'regex_sum_42.txt'
+    fname = 'regex_sum_326762.txt'
 
-fhandle = open(fname)
-list_nums = list()
-for line in fhandle:
-    line = line.rstrip()
-    words = line.split()
-    for word in words:
-        num = re.findall('[0-9]+', word)
-        if len(num) < 1:
-            continue
-        #num = int(num[0])
-        #print(num)
-        list_nums.append(num)
-    print(line, num)
+with open(fname, encoding='utf-8') as fhandle:
+    list_nums = list()
+    for line in fhandle:
+        line = line.rstrip()
+        words = line.split()
+        for word in words:
+            num = re.findall('[0-9]+', word)
+            if len(num) < 1:
+                continue
+            list_nums.append(num)
+        #print(line, num)
 
 print(list_nums)
 
 count = 0
-sum = 0
+total = 0
 for num in list_nums:
     while True:
         if len(num) > 1:
-            print(num[0])
+            #print(num[0])
             count += 1
-            sum += int(num[0])
+            total += int(num[0])
             del num[0]
         break
-    print(num[0])
+    #print(num[0])
     count += 1
-    sum += int(num[0])
+    total += int(num[0])
 
 
-print(f'There are {count} values with a sum= {sum}')
+print(f'There are {count} values with a sum= {total}')
+
+
+# optional list comprehension after:
+# print( sum( [ ****** *** * in **********('[0-9]+',**************************.read()) ] ) )
 
 
 
