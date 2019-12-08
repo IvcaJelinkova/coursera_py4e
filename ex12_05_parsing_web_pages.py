2c6e266fc3ab5fcfd2f5315870b0815810420f21
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-12-08 23:04:03 +0100

    ignore SSL certificate errors

--- ex12_05_parsing_web_pages.py
+++ ex12_05_parsing_web_pages.py
@@ -2,9 +2,15 @@
 
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import ssl      # remove unsupported url
+
+# ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter URL - ')
-html = urllib.request.urlopen(url).read()
+html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 # retrieve all of the anchor tags
