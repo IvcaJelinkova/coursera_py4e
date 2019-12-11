d2315d52f0dfc988804f86a514022c73d93e28a4
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-12-11 03:14:33 +0100

    add input repeating and position of tag from user

--- hw12_following_links_in_html.py
+++ hw12_following_links_in_html.py
@@ -30,22 +30,23 @@ soup = BeautifulSoup(html, 'html.parser')
 
 # retrieve all of the anchor tags
 tags = soup('a')
-count = 1
+user_count = int(input('Enter number of repeating: '))
+user_position = int(input('Enter position: '))
 position = 0
-for count in range(4):
+for count in range(user_count-1):
     for tag in tags:
         print('TAG:', tag)
         #print('\tURL:', tag.get('href', None))
         url = tag.get('href', None)
         print('\tContent:', tag.contents[0])
         position += 1
-        if position == 3:
+        if position == user_position:
             print('3. pozice!!! ')
             print(tag.contents[0], url)
             html = urlopen(url, context=ctx).read()
             soup = BeautifulSoup(html, 'html.parser')
             tags = soup('a')
-            print(position, count)
+            print(position, count+1)
             position = 0
             count += 1
             break
