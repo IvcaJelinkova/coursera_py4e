8796b1745958f6b8e45491338ca42079016a0e72
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-12-15 23:27:45 +0100

    two examples of parsing XML

---
+++ ex13_parsing_HML.py
@@ -0,0 +1,42 @@
+# ex13_parsing_HML.py
+
+import xml.etree.ElementTree as ET
+
+
+# 1. example:
+data = '''<person>
+    <name>Chuck</name>
+    <phone type="intl">
+        +1 734 303 4456
+    </phone>
+    <email hide="yes"/>
+</person>'''
+
+tree = ET.fromstring(data)
+print('Name:', tree.find('name').text)
+print('Attr:', tree.find('email').get('hide'))      # yes
+print()
+
+
+# 2. example:
+input = '''<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brend</name>
+        </user>
+    </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+for item in lst:
+    print('Name', item.find('name').text)
+    print('Id', item.find('id').text)
+    print('Attribute', item.get("x"))
+
