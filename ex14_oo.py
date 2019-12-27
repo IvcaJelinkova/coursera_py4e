a9d43d6d306afaf5842ec32e1c6d86e342a4088b
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-12-27 23:53:16 +0100

    init, del

--- ex14_oo.py
+++ ex14_oo.py
@@ -27,15 +27,23 @@ print(type(d))
 class PartyAnimal:
     x = 0
 
+    def __init__(self):
+        print('I am constructed')
+
     def party(self):
         self.x = self.x + 1
         print('So far', self.x)
+
+    def __del__(self):
+        print('I am destructed', self.x)
 
 an = PartyAnimal()
 
 an.party()
 an.party()
 an.party()
+an = 42
+print('an contains', an)
 
 print(type(an))
-print(dir(an))
+#print(dir(an))
