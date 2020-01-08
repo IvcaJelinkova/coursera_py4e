735ab8d3057fea5b7a800ba981d4befa234de3c7
Ivča Jelínková <jelinkova.ivana@gmail.com>
2020-01-08 23:36:57 +0100

    skeleton of script for saving my music database from xml to sql

---
+++ ex15_tracks.py
@@ -0,0 +1,47 @@
+# ex15_tracks.py
+
+import xml.etree.ElementTree AS ET
+import sqlite3
+
+# connection with dtb music.xml
+
+
+
+# make some fresh tables (Artist, Album, Track) using executescript()
+
+
+
+
+# input file
+
+
+
+
+# music.xml + def lookup(d, key)
+# <dict>
+# 		<key>207</key>      # <-- key!
+# 		<dict>
+# 			<key>Track ID</key><integer>207</integer>
+# 			<key>Name</key><string>Don't Let the Old Man In</string>
+# 			<key>Artist</key><string>Toby Keith</string>
+
+
+
+
+
+
+
+
+
+# parsing my music library and go to child I want (with detail about songs)
+
+
+
+
+
+
+
+# INSERT or IGNORE or REPLACE my tables with data, attention about generation foreign key (id)
+
+
+
